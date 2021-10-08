156a58c7cbc290804541ae7d115c752ba00c90f0
Madhunil <madpach95@gmail.com>
2021-10-08 05:55:48 -0400

    adding research file and scrapper class

--- text_scrapper.py
+++ text_scrapper.py
@@ -7,4 +7,21 @@ Created on Wed May 12 23:26:35 2021
 
 
 #%%
-a = 1 + 2
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium import webdriver
+import time
+import pandas as pd
+
+#Initializing the webdriver
+options = webdriver.ChromeOptions()
+
+#Uncomment the line below if you'd like to scrape without a new Chrome window every time.
+#options.add_argument('headless')
+
+#Change the path to where chromedriver is in your home folder.
+driver = webdriver.Chrome(executable_path="D:/NEU/My DS Projects/DS_salary_proj/chromedriver", options=options)
+driver.set_window_size(1120, 1000)
+
+url = 'https://www.sacred-texts.com/hin/rigveda/index.htm'
+driver.get(url)
+text = []
